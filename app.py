82a6bb242727c701e4f10795368db53c60939b42
princebyun <hkbyun@plani.co.kr>
2026-06-05 13:44:26 +0900

    chore: Nginx 주입 방식으로 변경됨에 따라 기존 코드 제거

--- app.py
+++ app.py
@@ -41,12 +41,6 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Google AdSense
-stc.html("""
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1248790153868284"
-         crossorigin="anonymous"></script>
-""", height=0)
-
 # 상단 언어 선택 버튼 (우측 상단 완전히 밀착)
 _, btn_col = st.columns([8.5, 1.5])
 with btn_col:
